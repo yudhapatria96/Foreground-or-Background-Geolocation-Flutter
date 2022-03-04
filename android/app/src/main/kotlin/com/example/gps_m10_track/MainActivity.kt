b22c936aafd4f5676e164b40c3b490841d63ea87
Yudha Patria <yudha.patria@mitra10.com>
2022-03-04 16:56:43 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/gps_m10_track/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gps_m10_track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:background_locator/background_locator.dart';
+import 'package:background_locator/location_dto.dart';
+import 'package:background_locator/settings/android_settings.dart';
+import 'package:background_locator/settings/ios_settings.dart';
+import 'package:background_locator/settings/locator_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:gps_m10_track/file_manager.dart';
+import 'package:gps_m10_track/location_callback_handler.dart';
+import 'package:gps_m10_track/location_service_repository.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:location_permissions/location_permissions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ReceivePort port = ReceivePort();
+
+  String logStr = '';
+  late bool isRunning;
+  late LocationDto? lastLocation = null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (IsolateNameServer.lookupPortByName(
+            LocationServiceRepository.isolateName) !=
+        null) {
+      IsolateNameServer.removePortNameMapping(
+          LocationServiceRepository.isolateName);
+    }
+
+    IsolateNameServer.registerPortWithName(
+        port.sendPort, LocationServiceRepository.isolateName);
+
+    port.listen(
+      (dynamic data) async {
+        await updateUI(data);
+      },
+    );
+    initPlatformState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> updateUI(LocationDto data) async {
+    // final log = await FileManager.readLogFile();
+
+    await _updateNotificationText(data);
+
+    setState(() {
+      if (data != null) {
+        lastLocation = data;
+      }
+      // logStr = log;
+    });
+  }
+
+  Future<void> _updateNotificationText(LocationDto data) async {
+    if (data == null) {
+      return;
+    }
+
+    await BackgroundLocator.updateNotificationText(
+        title: "new location received",
+        msg: "${DateTime.now()}",
+        bigMsg: "${data.latitude}, ${data.longitude}");
+  }
+
+  Future<void> initPlatformState() async {
+    print('Initializing...');
+    await BackgroundLocator.initialize();
+    // logStr = await FileManager.readLogFile();
+    print('Initialization done');
+    final _isRunning = await BackgroundLocator.isServiceRunning();
+    setState(() {
+      isRunning = _isRunning;
+    });
+    print('Running ${isRunning.toString()}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final start = SizedBox(
+      width: double.maxFinite,
+      child: ElevatedButton(
+        child: Text('Start'),
+        onPressed: () {
+          _onStart();
+        },
+      ),
+    );
+    final stop = SizedBox(
+      width: double.maxFinite,
+      child: ElevatedButton(
+        child: Text('Stop'),
+        onPressed: () {
+          onStop();
+        },
+      ),
+    );
+    final clear = SizedBox(
+      width: double.maxFinite,
+      child: ElevatedButton(
+        child: Text('Clear Log'),
+        onPressed: () {
+          // FileManager.clearLogFile();
+          setState(() {
+            logStr = '';
+          });
+        },
+      ),
+    );
+    String msgStatus = "-";
+    if (isRunning != null) {
+      if (isRunning) {
+        msgStatus = 'Is running';
+      } else {
+        msgStatus = 'Is not running';
+      }
+    }
+    final status = Text("Status: $msgStatus");
+
+    final log = Text(
+      logStr,
+    );
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter background Locator'),
+        ),
+        body: Container(
+          width: double.maxFinite,
+          padding: const EdgeInsets.all(22),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[start, stop, clear, status, log],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onStop() async {
+    await BackgroundLocator.unRegisterLocationUpdate();
+    final _isRunning = await BackgroundLocator.isServiceRunning();
+    setState(() {
+      isRunning = _isRunning;
+    });
+  }
+
+  void _onStart() async {
+    if (await _checkLocationPermission()) {
+      await _startLocator();
+      final _isRunning = await BackgroundLocator.isServiceRunning();
+
+      setState(() {
+        isRunning = _isRunning;
+        lastLocation = null;
+      });
+    } else {
+      // show error
+    }
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    final access = await LocationPermissions().checkPermissionStatus();
+    switch (access) {
+      case PermissionStatus.unknown:
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+        final permission = await LocationPermissions().requestPermissions(
+          permissionLevel: LocationPermissionLevel.locationAlways,
+        );
+        if (permission == PermissionStatus.granted) {
+          return true;
+        } else {
+          return false;
+        }
+        break;
+      case PermissionStatus.granted:
+        return true;
+        break;
+      default:
+        return false;
+        break;
+    }
+  }
+
+  Future<void> _startLocator() async {
+    Map<String, dynamic> data = {'countInit': 1};
+    return await BackgroundLocator.registerLocationUpdate(
+        LocationCallbackHandler.callback,
+        initCallback: LocationCallbackHandler.initCallback,
+        initDataCallback: data,
+        disposeCallback: LocationCallbackHandler.disposeCallback,
+        iosSettings: IOSSettings(
+            accuracy: LocationAccuracy.NAVIGATION, distanceFilter: 0),
+        autoStop: false,
+        androidSettings: AndroidSettings(
+            accuracy: LocationAccuracy.NAVIGATION,
+            interval: 5,
+            distanceFilter: 0,
+            client: LocationClient.google,
+            androidNotificationSettings: AndroidNotificationSettings(
+                notificationChannelName: 'Location tracking',
+                notificationTitle: 'Start Location Tracking',
+                notificationMsg: 'Track location in background',
+                notificationBigMsg:
+                    'Background location is on to keep the app up-to-date with your location. This is required for main features to work properly when the app is not running.',
+                notificationIconColor: Colors.grey,
+                notificationTapCallback:
+                    LocationCallbackHandler.notificationCallback)));
+  }
+}
